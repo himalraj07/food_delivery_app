fddbf8f7a0360b505567315439b057732a22f028
Himal Raj Bhusal <139055215+himalraj07@users.noreply.github.com>
2025-06-02 00:14:47 +0545

    update

--- src/features/auth/SplashScreen.tsx
+++ src/features/auth/SplashScreen.tsx
@@ -1,12 +1,28 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import {View, Text, StatusBar, Platform, Image} from 'react-native';
+import React, {FC} from 'react';
+import {useStyles} from 'react-native-unistyles';
+import {splashStyles} from '@unistyles/authStyles';
+import Animated, {FadeInDown} from 'react-native-reanimated';
 
-const SplashScreen = () => {
+const SplashScreen: FC = () => {
+  const {styles} = useStyles(splashStyles);
   return (
-    <View>
-      <Text>SplashScreen</Text>
+    <View style={styles.container}>
+      <StatusBar hidden={Platform.OS !== 'android'} />
+      <Image
+        source={require('@assets/images/logo_t.png')}
+        style={styles.logoImage}
+      />
+      <Animated.View
+        style={styles.animatedContainer}
+        entering={FadeInDown.delay(400).duration(800)}>
+        <Image
+          source={require('@assets/images/tree.png')}
+          style={styles.treeImage}
+        />
+      </Animated.View>
     </View>
-  )
-}
+  );
+};
 
-export default SplashScreen
+export default SplashScreen;
