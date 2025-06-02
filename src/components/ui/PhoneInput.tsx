9ec1f945fc5fc850f530098fc09fd75657845933
Himal Raj Bhusal <139055215+himalraj07@users.noreply.github.com>
2025-06-02 18:52:30 +0545

    update

---
+++ src/components/ui/PhoneInput.tsx
@@ -0,0 +1,54 @@
+import CustomText from '@components/global/CustomText';
+import {Colors} from '@unistyles/Constants';
+import {phoneStyles} from '@unistyles/phoneStyles';
+import React, {FC} from 'react';
+import {Pressable, TextInput, View} from 'react-native';
+import {useStyles} from 'react-native-unistyles';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
+interface PhoneInputProps {
+  value: string;
+  onChange: (text: string) => void;
+  onFocus: () => void;
+  onBlur: () => void;
+}
+
+const PhoneInput: FC<PhoneInputProps> = ({
+  value,
+  onChange,
+  onBlur,
+  onFocus,
+}) => {
+  const {styles} = useStyles(phoneStyles);
+
+  return (
+    <View style={styles.container}>
+      <Pressable style={styles.countryPickerContainer}>
+        <CustomText variant="h4">🇳🇵</CustomText>
+        <Ionicons
+          iconFamily="Ionicons"
+          name="caret-down-sharp"
+          color={Colors.lightText}
+          size={18}
+        />
+      </Pressable>
+
+      <View style={styles.phoneInputContainer}>
+        <CustomText fontFamily="Okra-Bold">+977</CustomText>
+        <TextInput
+          placeholder="Enter Mobile Number"
+          keyboardType="phone-pad"
+          value={value}
+          maxLength={10}
+          placeholderTextColor={Colors.lightText}
+          onChangeText={onChange}
+          onFocus={onFocus}
+          onBlur={onBlur}
+          style={styles.input}
+        />
+      </View>
+    </View>
+  );
+};
+
+export default PhoneInput;
--- src/features/auth/LoginScreen.tsx
+++ src/features/auth/LoginScreen.tsx
@@ -1,13 +1,52 @@
-import {View, StatusBar, Platform, Image} from 'react-native';
-import Animated from 'react-native-reanimated';
-import React, {FC} from 'react';
-import {loginStyles} from '@unistyles/authStyles';
-import {useStyles} from 'react-native-unistyles';
 import CustomText from '@components/global/CustomText';
 import BreakerText from '@components/ui/BreakerText';
+import PhoneInput from '@components/ui/PhoneInput';
+import SocialLogin from '@components/ui/SocialLogin';
+import {loginStyles} from '@unistyles/authStyles';
+import {resetAndNavigate} from '@utils/NavigationUtils';
+import useKeyboardOffsetHeight from '@utils/useKeyboardOffsetHeight';
+import React, {FC, useEffect, useRef, useState} from 'react';
+import {
+  ActivityIndicator,
+  Image,
+  Platform,
+  StatusBar,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import {Animated} from 'react-native';
+import {useStyles} from 'react-native-unistyles';
 
 const LoginScreen: FC = () => {
+  const animatedValue = useRef(new Animated.Value(0)).current;
+  const keyboardOffsetHeight = useKeyboardOffsetHeight();
   const {styles} = useStyles(loginStyles);
+  const [phone, setPhone] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    if (keyboardOffsetHeight == 0) {
+      Animated.timing(animatedValue, {
+        toValue: 0,
+        duration: 300,
+        useNativeDriver: false,
+      }).start();
+    } else {
+      Animated.timing(animatedValue, {
+        toValue: -keyboardOffsetHeight * 0.25,
+        duration: 500,
+        useNativeDriver: false,
+      }).start();
+    }
+  }, [keyboardOffsetHeight]);
+
+  const handleLogin = async () => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+      resetAndNavigate('UserBottomTab');
+    }, 2000);
+  };
 
   return (
     <View style={styles.container}>
@@ -22,13 +61,48 @@ const LoginScreen: FC = () => {
         bounces={false}
         keyboardShouldPersistTaps="handled"
         keyboardDismissMode="on-drag"
+        style={{transform: [{translateY: animatedValue}]}}
         contentContainerStyle={styles.bottomContainer}>
         <CustomText fontFamily="Okra-Bold" variant="h2" style={styles.title}>
           Nepal's #1 Food Delivery and Dining App
         </CustomText>
 
         <BreakerText text="Log in or sign up" />
+
+        <PhoneInput
+          onFocus={() => {}}
+          onBlur={() => {}}
+          value={phone}
+          onChange={setPhone}
+        />
+
+        <TouchableOpacity
+          style={styles.buttonContainer}
+          disabled={loading}
+          onPress={handleLogin}
+          activeOpacity={0.8}>
+          {loading ? (
+            <ActivityIndicator size="small" color="#fff" />
+          ) : (
+            <CustomText color="#fff" fontFamily="Okra-Medium" variant="h5">
+              Continue
+            </CustomText>
+          )}
+        </TouchableOpacity>
+
+        <BreakerText text="or" />
+
+        <SocialLogin />
       </Animated.ScrollView>
+
+      <View style={styles.footer}>
+        <CustomText>By continuing, you agree to our</CustomText>
+        <View style={styles.footerTextContainer}>
+          <CustomText style={styles.footerText}>Terms of Service</CustomText>
+          <CustomText style={styles.footerText}>Privacy Policy</CustomText>
+          <CustomText style={styles.footerText}>Content Policy</CustomText>
+        </View>
+      </View>
     </View>
   );
 };
