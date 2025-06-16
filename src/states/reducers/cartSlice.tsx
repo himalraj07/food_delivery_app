e98df4529ca8301be639a3373128692b2ed031c2
Himal Raj Bhusal <139055215+himalraj07@users.noreply.github.com>
2025-06-16 10:22:39 +0545

    update

--- src/states/reducers/cartSlice.tsx
+++ src/states/reducers/cartSlice.tsx
@@ -120,7 +120,86 @@ export const cartSlice = createSlice({
           customizationOptions: any[];
         };
       }>,
-    ) => {},
+    ) => {
+      const {restaurant, item, customization} = action.payload;
+      const existingRestaurantCart = state.carts.find(
+        cart => cart.restaurant.id === restaurant.id,
+      );
+
+      if (existingRestaurantCart) {
+        const existingItem = existingRestaurantCart?.items?.find(
+          cartItem => cartItem?.id === item?.id,
+        ) as any;
+
+        if (existingItem) {
+          const existingCustomizationIndex =
+            existingItem?.customizations?.findIndex(
+              (cust: any) =>
+                JSON.stringify(cust.customizationOptions) ===
+                JSON.stringify(customization.customizationOptions),
+            );
+
+          if (
+            existingCustomizationIndex !== undefined &&
+            existingCustomizationIndex !== -1
+          ) {
+            const existingCustomization =
+              existingItem?.customizations[existingCustomizationIndex];
+            existingCustomization.quantity += customization?.quantity;
+            existingCustomization.cartPrice += customization?.price;
+          } else {
+            const newCustomizationId = uuid();
+            existingItem?.customizations?.push({
+              id: newCustomizationId,
+              ...customization,
+              quantity: customization?.quantity,
+              cartPrice: customization?.price,
+              price: customization?.price / customization?.quantity,
+            });
+          }
+
+          existingItem.quantity += customization?.quantity;
+          existingItem.cartPrice =
+            (existingItem?.cartPrice || 0) + customization?.price;
+        } else {
+          const newCustomizationId = `c1`;
+          existingRestaurantCart.items.push({
+            ...item,
+            quantity: customization.quantity,
+            cartPrice: customization.price,
+            customizations: [
+              {
+                id: newCustomizationId,
+                ...customization,
+                quantity: customization.quantity,
+                cartPrice: customization.price,
+                price: customization.price / customization.quantity,
+              },
+            ],
+          });
+        }
+      } else {
+        const newCustomizationId = `c1`;
+        state.carts.push({
+          restaurant,
+          items: [
+            {
+              ...item,
+              quantity: customization?.price,
+              customizations: [
+                {
+                  id: newCustomizationId,
+                  ...customization,
+                  quantity: customization?.quantity,
+                  cartPrice: customization.price,
+                  price: customization.price / customization.quantity,
+                },
+              ],
+            },
+          ],
+        });
+      }
+    },
 
     removeCustomizableItem: (
       state,
@@ -129,7 +208,50 @@ export const cartSlice = createSlice({
         itemId: string;
         customizationId: string;
       }>,
-    ) => {},
+    ) => {
+      const {restaurant_id, itemId, customizationId} = action.payload;
+      const restautantCart = state?.carts?.find(
+        cart => cart?.restaurant?.id === restaurant_id,
+      );
+
+      if (!restautantCart) return;
+
+      const item = restautantCart?.items?.find(
+        cartItem => cartItem?.id === itemId,
+      );
+
+      if (!item) return;
+
+      const customizationIndex = item?.customizations?.findIndex(
+        cust => cust?.id === customizationId,
+      ) as number;
+
+      if (customizationIndex !== -1 && item?.customizations) {
+        const customization = item.customizations[customizationIndex];
+
+        if (customization?.quantity > 1) {
+          customization.quantity -= 1;
+          customization.cartPrice -= customization?.price;
+        } else {
+          item?.customizations?.splice(customizationIndex, 1);
+        }
+
+        item.quantity -= 1;
+        item.cartPrice = (item?.cartPrice || 0) - customization?.price;
+
+        if (item?.quantity === 0 || item?.customizations?.length === 0) {
+          restautantCart.items = restautantCart?.items?.filter(
+            cartItem => cartItem.id !== itemId,
+          );
+        }
+
+        if (restautantCart?.items?.length === 0) {
+          state.carts = state.carts?.filter(
+            cart => cart?.restaurant?.id !== restaurant_id,
+          );
+        }
+      }
+    },
 
     updateCustomizableItem: (
       state,
@@ -143,11 +265,65 @@ export const cartSlice = createSlice({
           customizationOptions: any[];
         };
       }>,
-    ) => {},
+    ) => {
+      const {restaurant_id, itemId, customizationId, newCustomization} =
+        action.payload;
+      const restaurantCart = state.carts.find(
+        cart => cart.restaurant.id === restaurant_id,
+      );
+      if (!restaurantCart) return;
+
+      const item = restaurantCart.items.find(
+        cartItem => cartItem.id === itemId,
+      );
+      if (!item || !item.customizations) return;
+
+      const matchingCustomizationIndex = item?.customizations?.findIndex(
+        (cust: any) =>
+          cust?.id !== customizationId &&
+          JSON.stringify(cust.customizationOptions) ===
+            JSON.stringify(newCustomization.customizationOptions),
+      );
+
+      const targetCustomizationIndex = item?.customizations?.findIndex(
+        cust => cust.id === customizationId,
+      );
+      if (targetCustomizationIndex === -1) return;
+
+      const targetCustomization =
+        item?.customizations[targetCustomizationIndex];
+
+      if (matchingCustomizationIndex !== -1) {
+        const matchingCustomization =
+          item.customizations[matchingCustomizationIndex];
+
+        matchingCustomization.quantity += newCustomization?.quantity;
+        matchingCustomization.cartPrice += newCustomization.price;
+
+        item?.customizations?.splice(targetCustomizationIndex, 1);
+      } else {
+        targetCustomization.quantity = newCustomization.quantity;
+        targetCustomization.cartPrice = newCustomization.price;
+        targetCustomization.price =
+          newCustomization.price / newCustomization.quantity;
+        targetCustomization.customizationOptions =
+          newCustomization.customizationOptions;
+      }
+
+      item.quantity = item?.customizations?.reduce(
+        (sum, cust) => sum + cust.quantity,
+        0,
+      );
+      item.cartPrice = item?.customizations?.reduce(
+        (sum, cust) => sum + cust.cartPrice,
+        0,
+      );
+    },
 
     clearAllCarts: state => {
       state.carts = [];
     },
+
     clearRestaurantCart: (
       state,
       action: PayloadAction<{restaurant_id: string}>,
