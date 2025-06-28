0303e17fabbbc6c03a7147d2d28b2595eff0b0c1
Himal Raj Bhusal <139055215+himalraj07@users.noreply.github.com>
2025-06-28 09:29:16 +0545

    removeItemModel Completed

--- src/components/modal/RemoveItemModal.tsx
+++ src/components/modal/RemoveItemModal.tsx
@@ -1,10 +1,52 @@
-import {View, Text} from 'react-native';
-import React from 'react';
+import {View, Text, SafeAreaView, ScrollView} from 'react-native';
+import React, {FC, useEffect} from 'react';
+import {useAppSelector} from '@states/reduxHook';
+import {selectRestaurantCartItem} from '@states/reducers/cartSlice';
+import {useStyles} from 'react-native-unistyles';
+import {modelStyles} from '@unistyles/modelStyles';
+import CustomText from '@components/global/CustomText';
+import MiniFoodCard from '@components/restaurant/MiniFoodCard';
 
-const RemoveItemModal = () => {
+const RemoveItemModal: FC<{
+  item: any;
+  restaurant: any;
+  closeModal: () => void;
+}> = ({item, restaurant, closeModal}) => {
+  const cartItem = useAppSelector(
+    selectRestaurantCartItem(restaurant?.id, item?.id),
+  );
+  const {styles} = useStyles(modelStyles);
+
+  useEffect(() => {
+    if (!cartItem) {
+      closeModal();
+    }
+  }, [cartItem]);
   return (
     <View>
-      <Text>RemoveItemModal</Text>
+      <View style={styles.noShadowHeaderContainer}>
+        <View style={styles.flexRowGap}>
+          <CustomText fontFamily="Okra-Bold" fontSize={13}>
+            Customizations for {item?.name}
+          </CustomText>
+        </View>
+      </View>
+
+      <ScrollView
+        contentContainerStyle={styles?.scrollContainerWhiteBackground}>
+        {cartItem?.customizations?.map((cus, index) => {
+          return (
+            <MiniFoodCard
+              item={item}
+              cus={cus}
+              key={cus?.id}
+              restaurant={restaurant}
+            />
+          );
+        })}
+      </ScrollView>
+
+      <SafeAreaView />
     </View>
   );
 };
--- src/components/modal/RepeatItemModal.tsx
+++ src/components/modal/RepeatItemModal.tsx
@@ -1,12 +1,64 @@
-import {View, Text} from 'react-native';
-import React from 'react';
+import {View, TouchableOpacity, ScrollView} from 'react-native';
+import React, {FC, useEffect} from 'react';
+import {selectRestaurantCartItem} from '@states/reducers/cartSlice';
+import {useAppSelector} from '@states/reduxHook';
+import {useStyles} from 'react-native-unistyles';
+import {modelStyles} from '@unistyles/modelStyles';
+import CustomText from '@components/global/CustomText';
+import {Colors} from '@unistyles/Constants';
+import MiniFoodCard from '@components/restaurant/MiniFoodCard';
 
-const RepeatItemModal = () => {
+const RepeatItemModal: FC<{
+  item: any;
+  restaurant: any;
+  onOpenAddModal: () => void;
+  closeModal: () => void;
+}> = ({item, onOpenAddModal, restaurant, closeModal}) => {
+  const cartItem = useAppSelector(
+    selectRestaurantCartItem(restaurant?.id, item?.id),
+  );
+  const {styles} = useStyles(modelStyles);
+
+  useEffect(() => {
+    if (!cartItem) {
+      closeModal();
+    }
+  }, [cartItem]);
+
   return (
     <View>
-      <Text>RepeatItemModal</Text>
+      <View style={styles.noShadowHeaderContainer}>
+        <View style={styles.flexRowGap}>
+          <CustomText fontFamily="Okra-Bold" fontSize={13}>
+            Repeat last used customization?
+          </CustomText>
+        </View>
+      </View>
+
+      <ScrollView contentContainerStyle={styles.scrollContainerWhiteBackground}>
+        {cartItem?.customizations?.map((cus, index) => {
+          return (
+            <MiniFoodCard
+              item={item}
+              cus={cus}
+              key={index}
+              restaurant={restaurant}
+            />
+          );
+        })}
+      </ScrollView>
+
+      <View style={styles.noShadowFooterContainer}>
+        <TouchableOpacity onPress={onOpenAddModal}>
+          <CustomText
+            fontFamily="Okra-Bold"
+            color={Colors.active}
+            fontSize={11}>
+            + Add new customization
+          </CustomText>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
-
 export default RepeatItemModal;
