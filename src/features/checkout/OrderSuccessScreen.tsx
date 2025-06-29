9294996d893804d5bf5ad4eb908667fcadc80cf8
Himal Raj Bhusal <139055215+himalraj07@users.noreply.github.com>
2025-06-29 21:33:51 +0545

    Completed

--- src/features/checkout/OrderSuccessScreen.tsx
+++ src/features/checkout/OrderSuccessScreen.tsx
@@ -1,12 +1,89 @@
-import {View, Text} from 'react-native';
-import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+import React, {FC, useEffect} from 'react';
+import {useRoute} from '@react-navigation/native';
+import {useAppDispatch} from '@states/reduxHook';
+import {replace} from '@utils/NavigationUtils';
+import {clearRestaurantCart} from '@states/reducers/cartSlice';
+import LottieView from 'lottie-react-native';
+import {Colors, screenWidth} from '@unistyles/Constants';
+import CustomText from '@components/global/CustomText';
 
-const OrderSuccessScreen = () => {
+const OrderSuccessScreen: FC = () => {
+  const route = useRoute() as any;
+  const dispatch = useAppDispatch();
+  const restaurant = route?.params?.restaurant;
+
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      replace('UserBottomTab');
+      dispatch(clearRestaurantCart({restaurant_id: restaurant?.id}));
+    }, 2300);
+
+    return () => clearTimeout(timeoutId);
+  }, []);
+
   return (
-    <View>
-      <Text>OrderSuccessScreen</Text>
+    <View style={styles.container}>
+      <LottieView
+        source={require('@assets/animations/confirm.json')}
+        autoPlay
+        duration={2000}
+        loop={false}
+        speed={1}
+        style={styles.LottieView}
+        enableMergePathsAndroidForKitKatAndAbove={true}
+        hardwareAccelerationAndroid
+      />
+      <CustomText
+        fontSize={12}
+        fontFamily="Okra-Bold"
+        style={styles.orderPlacedText}>
+        ORDER PLACED
+      </CustomText>
+      <View style={styles.deliveryContainer}>
+        <CustomText
+          variant="h4"
+          fontFamily="Okra-Bold"
+          style={styles.deliveryText}>
+          Delivering to Home
+        </CustomText>
+      </View>
+      <CustomText
+        fontSize={12}
+        style={styles.addressText}
+        fontFamily="Okra-Regular">
+        Pochinki, Erangel ({restaurant?.name})
+      </CustomText>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    flex: 1,
+  },
+  LottieView: {
+    width: screenWidth * 0.6,
+    height: 150,
+  },
+  orderPlacedText: {
+    opacity: 0.4,
+  },
+  deliveryContainer: {
+    borderBottomWidth: 2,
+    paddingBottom: 4,
+    marginBottom: 5,
+    borderColor: Colors.active,
+  },
+  deliveryText: {
+    marginTop: 15,
+    borderColor: Colors.active,
+  },
+  addressText: {
+    opacity: 0.8,
+  },
+});
 
 export default OrderSuccessScreen;
